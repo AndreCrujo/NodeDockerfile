61ad333e32bfb80c14b51dfc4e6923a1441e4e62
andre@tradingeconomics.com <andre@tradingeconomics.com>
2015-01-16 09:23:21 +0000

    -

--- git-log-prompt.sh
+++ git-log-prompt.sh
@@ -4,6 +4,9 @@ cd home
 git init
 #TEMP=`git clone https://$@@github.com/AntonioJFSousa/Trading.git`
 git clone https://$@@github.com/AntonioJFSousa/Trading.git
+
+cd Trading/ui_angular_trading
+node app.js
 #echo $TEMP
 #exec $TEMP
 #echo "o
