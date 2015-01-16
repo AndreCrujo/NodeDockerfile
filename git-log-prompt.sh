68e3f8ccecbb35ea6d141832cef69ea7898dad6c
andre@tradingeconomics.com <andre@tradingeconomics.com>
2015-01-16 10:02:08 +0000

    -

--- git-log-prompt.sh
+++ git-log-prompt.sh
@@ -6,4 +6,5 @@ git init
 git clone https://$@@github.com/AntonioJFSousa/Trading.git
 
 cd Trading/ui_angular_trading
+/tmp/node-v0.10.29-linux-x64/bin/npm install
 /tmp/node-v0.10.29-linux-x64/bin/node app.js
